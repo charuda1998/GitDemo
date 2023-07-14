71236161dd27b7ecc358da2196ac378cc232d501
charuda1998 <charuda.c@marketsimplified.com>
2023-07-14 11:52:28 +0530

    first commit

---
+++ India.java
@@ -0,0 +1,31 @@
+package com.example;
+
+public class India {
+    public void Speaking() {
+
+        System.out.println("i can speak indian language");
+    }
+}
+class Delhi extends  India{
+    public void Speaking() {
+
+        System.out.println("i can speak hindi");
+    }
+
+}
+class Kerala extends  India{
+    public void Speaking() {
+
+        System.out.println("i can speak Malayalam");
+    }
+
+}
+
+class TamilNadu extends  India{
+    public void Speaking() {
+
+        System.out.println("i can speak Tamil");
+    }
+
+}
+
---
+++ Person.java
@@ -0,0 +1,21 @@
+public class Person {
+    String name;
+    int phone;
+    public void showAddress(){
+        System.out.println("name:" +name);
+        System.out.println("phone:" +phone);
+
+    }
+}
+
+class Teachers extends Person {
+
+}
+class Staffs extends Person{
+
+}
+class Students extends Person{
+
+}
+
+
